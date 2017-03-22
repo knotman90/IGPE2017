b62f35927a3dd1418d52a6d8057d5900f0643809
Davide Spataro <davide90.spataro@gmail.com>
2017-03-22 12:31:36 +0100

    Big Integer example added

--- code/src/BookStore/Book.java
+++ code/src/BookStore/Book.java
@@ -3,9 +3,11 @@ package BookStore;
 import java.util.Date;
 
 /**
-* A onject representing a simplified version of a book.
+* A object representing a simplified version of a book.
 * A book has a Date of publishing, an author, a number of pages expressed as an integer and 
 * a String representing the International Standard Book Number.
+* 
+* @author Davide Spataro
 */
 public class Book {
 	
@@ -13,6 +15,54 @@ public class Book {
 	private Date year;
 	private int npages;
 	private String ISBN;
+	/**
+	 * @return the author
+	 */
+	public String getAuthor() {
+		return author;
+	}
+	/**
+	 * @param author the author to set
+	 */
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	/**
+	 * @return the year
+	 */
+	public Date getYear() {
+		return year;
+	}
+	/**
+	 * @param year the year to set
+	 */
+	public void setYear(Date year) {
+		this.year = year;
+	}
+	/**
+	 * @return the npages
+	 */
+	public int getNpages() {
+		return npages;
+	}
+	/**
+	 * @param npages the npages to set
+	 */
+	public void setNpages(int npages) {
+		this.npages = npages;
+	}
+	/**
+	 * @return the iSBN
+	 */
+	public String getISBN() {
+		return ISBN;
+	}
+	/**
+	 * @param iSBN the iSBN to set
+	 */
+	public void setISBN(String iSBN) {
+		ISBN = iSBN;
+	}
 	
 
 }
--- code/src/Various/Various.java
+++ code/src/Various/Various.java
@@ -5,73 +5,121 @@ import java.math.BigInteger;
 public class Various {
 
 
-
-	public static void main(String[] args) {
+	public static void ipowTest(){
+		System.out.println("IPOW: \n");
 		int n=2;
 		long s = System.currentTimeMillis();
 		
-		for(int i=0;i<500000 ; i++)
-			if(i==400000)
-				System.out.println(n+"^"+i + "\t"+ ipow(n, i));
+		for(int i=0;i<100 ; i++)
+			System.out.println(n+"^" + i +"\t"+ ipow(n,i));
+
+		
 		long e = System.currentTimeMillis();
-
 		System.out.println((double)(e-s)/1000.0);
+
 	}
-	//	public static int ipow(final int n, final int k){
-	//		if(k==0)
-	//			return 1;
-	//		int ans =n;
-	//		for(int i=1; i<k ; i++){
-	//			ans*=n;
-	//		}
-	//		return ans;
-	//	}
 
-	//	public static long ipow(final int n, final int k){
-	//		if(k==0)
-	//			return 1;
-	//		if(k==1)
-	//			return n;
-	//		if((k&1)==1)
-	//			return ipow(n,k-1)*n;
-	//		
-	//		return ipow(n,k/2)*ipow(n,k/2);
-	//		
-	//	}
+	public static void lpowTest(){
+		System.out.println("IPOW: \n");
+		int n=2;
+		long s = System.currentTimeMillis();
+		
+		for(int i=0;i<100 ; i++)
+			System.out.println(n+"^" + i +"\t"+ lpow(n,i));
 
-	//	public static long ipow(final long n, final long k){
-	//		if(k==0)
-	//			return 1;
-	//		long ans =n;
-	//		for(int i=1; i<k ; i++){
-	//			ans*=n;
-	//		}
-	//		return ans;
-	//	}
+		
+		long e = System.currentTimeMillis();
+		System.out.println((double)(e-s)/1000.0);
 
-//	public static BigInteger ipow(final long n, final long k){
-//		if(k==0)
-//			return BigInteger.ONE;
-//		BigInteger bin = BigInteger.valueOf(n);
-//		BigInteger bians = bin;
-//		for(int i=1; i<k ; i++){
-//			bians=bians.multiply(bin);
-//		}
-//		return bians;
-//	}
+	}
+	
+	public static void bipowRTest(){
+		System.out.println("BIPOWR: \n");
+		int n=2;
+		long s = System.currentTimeMillis();
+		BigInteger ans = BigInteger.ZERO;
+		for(int i=0;i<7000 ; i++)
+			ans=ans.add(bipowR(n, i));
 
-	public static BigInteger ipow(final int n, final int k){
+		System.out.println(n+"^" + "\t"+ ans);
+		long e = System.currentTimeMillis();
+		System.out.println((double)(e-s)/1000.0);
+
+	}
+
+
+	public static void bipowTest(){
+		System.out.println("BIPOW: \n");
+		int n=2;
+		long s = System.currentTimeMillis();
+		BigInteger ans = BigInteger.ZERO;
+		for(int i=0;i<7000 ; i++){
+			//System.out.println(i+" "+bipow(n, i));
+			ans=ans.add(bipow(n, i));
+		}
+
+		System.out.println(n+"^" + "\t"+ ans);
+		long e = System.currentTimeMillis();
+		System.out.println((double)(e-s)/1000.0);
+
+	}
+
+
+	public static int ipow(final int n, final int k){
+		if(k==0)
+			return 1;
+		int ans =n;
+		for(int i=1; i<k ; i++){
+			ans*=n;
+		}
+		return ans;
+	}
+
+
+
+	public static long lpow(final int n, int  k){
+
+		if(k==0)
+			return 1;
+		long ans =n;
+		for(int i=1; i<k ; i++){
+			ans*=n;
+		}
+		return ans;
+	}
+
+	public static BigInteger bipow(final long n, final long k){
+		if(k==0)
+			return BigInteger.ONE;
+		BigInteger bin = BigInteger.valueOf(n);
+		BigInteger bians = bin;
+		for(int i=1; i<k ; i++){
+			bians=bians.multiply(bin);
+		}
+		return bians;
+	}
+
+	public static BigInteger bipowR(final int n, final int k){
 		if(k==0)
 			return BigInteger.ONE;
 		if(k==1)
 			return BigInteger.valueOf(n);
 		if((k&1)==1)
-			return ipow(n,k-1).multiply(BigInteger.valueOf(n));
+			return bipowR(n,k-1).multiply(BigInteger.valueOf(n));
 
-		BigInteger res = ipow(n,k/2); 
+		BigInteger res = bipowR(n,k/2); 
 		return (res).multiply(res);
 
 	}
 
-
+	
+	/**
+	 * MAIN
+	 */
+	public static void main(String[] args) {
+		ipowTest();
+		//lpowTest();
+		//bipowTest();
+		//bipowRTest();
+	}
 }
